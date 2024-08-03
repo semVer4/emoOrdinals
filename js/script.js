3857cd2e605d6424b04121409d14569fa2ae7e4c
Nikita <73615368+semVer4@users.noreply.github.com>
2024-08-03 17:58:18 +0300

    update fullscreen

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ function showTab(tabId) {
     const activeTab = document.getElementById(tabId);
     activeTab.style.display = 'block';
 }
+
+document.getElementById('openBtn').addEventListener('click', function() {
+    document.getElementById('fullscreenOverlay').style.display = 'flex';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('fullscreenOverlay').style.display = 'none';
+});
+
+document.getElementById('fullscreenOverlay').addEventListener('click', function(event) {
+    if (event.target === this) {
+        this.style.display = 'none';
+    }
+});
